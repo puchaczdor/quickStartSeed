4f1c97de87f345dc228b8cdecc85b8b6834a9ab5
puchaczdor <puchaczdor@gmail.com>
2017-01-19 11:54:21 -0300

    commit con Atom , agrego gitignore

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule ],   // NgIf , NgFor y otras directivas
+  declarations: [ AppComponent ],    // root component
+  bootstrap:    [ AppComponent ]     // selector <my-app> del index.html
 })
 export class AppModule { }
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,19 @@
+ // JIT Just-in-Time compiler : Dinamic bootstrapping
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+// AOT Ahead-Of-Time compiler: Static bootstrapping
+
+// The browser platform without a compiler
+// import { platformBrowser } from '@angular/platform-browser';
+//
+// // The app module factory produced by the static offline compiler
+// import { AppModuleNgFactory } from './app.module.ngfactory';
+//
+// // Launch with the app module factory.
+// platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
